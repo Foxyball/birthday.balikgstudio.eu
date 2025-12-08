95aabda24222dfe5ae6443cff0fbdfaed51b8aaf
hsabev <thefoxybg@gmail.com>
2025-12-08 19:52:29 +0000

    add contacts migration #1

--- database/migrations/2025_12_08_193525_create_contacts_table.php
+++ database/migrations/2025_12_08_193525_create_contacts_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->date('birthday');
+             $table->text('image')->nullable();
             $table->timestamps();
         });
     }
