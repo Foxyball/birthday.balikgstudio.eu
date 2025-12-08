a22bcc2a34a4fb3a680125972ffe7143cefdf03c
hsabev <thefoxybg@gmail.com>
2025-12-08 21:27:49 +0000

    setup vite for dev #7

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        https: true,
+        host: 'localhost',
+        port: 5173,
+        hmr: {
+            host: 'birthday.balikgstudio.eu', // your public domain
+            protocol: 'wss', // use websocket secure for HTTPS
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
