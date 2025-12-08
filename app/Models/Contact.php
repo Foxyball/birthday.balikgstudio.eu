ae1d029921b4b200a559a2adc3ed87be5c4bb4e8
hsabev <thefoxybg@gmail.com>
2025-12-08 19:59:34 +0000

    add fillable contacts #1

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'name',
+        'email',
+        'phone',
+        'birthday',
+        'image',
+    ];
 }
