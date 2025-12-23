a5374af040032723636ce3e61202e1fc0b0168f2
hsabev <thefoxybg@gmail.com>
2025-12-23 12:36:04 +0000

    change image of auth #21

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -4,14 +4,16 @@ export default function AuthLayout({
     children,
     title,
     description,
+    imageUrl,
     ...props
 }: {
     children: React.ReactNode;
     title: string;
     description: string;
+    imageUrl?: string;
 }) {
     return (
-        <AuthLayoutTemplate title={title} description={description} {...props}>
+        <AuthLayoutTemplate title={title} description={description} imageUrl={imageUrl} {...props}>
             {children}
         </AuthLayoutTemplate>
     );
